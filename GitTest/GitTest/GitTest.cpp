9efe346e56c176f91b2a0fd7918f6c540b6a480e
Patryk <patryk@op.pl>
2015-12-03 11:13:00 +0100

    opis

---
+++ GitTest/GitTest/GitTest.cpp
@@ -0,0 +1,17 @@
+// GitTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "A" << endl;
+	cin.get();
+    return 0;
+}
+
